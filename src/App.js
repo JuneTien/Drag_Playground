ebcd75ece875d46df78e4f39555b0f938418cbec
June Tien <junetien@Junede-MacBook-Pro.local>
2017-08-27 22:43:37 +0800

    Add drag component.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import MotionBlock from './MotionBlock';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MotionBlock/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MotionBlock.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import './App.css';
+
+let scroll;
+let startX = 0;
+let startY = 0;
+
+let origX = 0;
+let origY = 0;
+let deltaX = 0;
+let deltaY = 0;
+
+// let finalPosLeft = 0;
+// let finalPosTop = 0;
+
+class MotionBlock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  getScrollOffsets = (w) => {
+    w = w || window;
+    if(w.pageXOffset != null) return {x: w.pageXOffset, y: w.pageYOffset};
+    var d = w.document;
+    if(document.compatMode === "CSS1Compat")
+      return {x: d.documentElement.scrollLeft, y: d.documentElement.scrollTop};
+
+    return {x: d.body.scrollLeft, y: d.body.scrollTop};
+  }
+
+  moveHandler = (e) => {
+    if(!e) e = window.event;
+    scroll = this.getScrollOffsets();
+    this.refs.thisMount.style.left = (e.clientX + scroll.x - deltaX)+"px";
+    this.refs.thisMount.style.top = (e.clientY + scroll.y - deltaY)+"px";
+  }
+
+  upHandler = (e) => {
+    if (!e) e = window.event;
+    if (document.removeEventListener) {
+      document.removeEventListener("mouseup", this.upHandler, true);
+      document.removeEventListener("mousemove", this.moveHandler, true);
+    } else if (document.detachEvnet) {
+      this.refs.thisMount.detachEvent("onlosecapture", this.upHandler);
+      this.refs.thisMount.detachEvent("onmouseup", this.upHandler);
+      this.refs.thisMount.detachEvent("onmousemove", this.moveHandler);
+    }
+
+    if(e.stopPropagation) e.stopPropagation();
+    else e.cancelBubble = true;
+  }
+
+  onDrag = (e) => {
+    document.ontouchmove = function (e) {
+      e.preventDefault();
+    };
+
+    scroll = this.getScrollOffsets();
+    startX = e.clientX + scroll.x;
+    startY = e.clientY + scroll.y;
+
+    origX = this.refs.thisMount.offsetLeft;
+    origY = this.refs.thisMount.offsetTop;
+    deltaX = startX - origX;
+    deltaY = startY - origY;
+
+    document.addEventListener("mousemove", this.moveHandler, true);
+    document.addEventListener("mouseup", this.upHandler, true);    
+
+    if (document.addEventListener) {
+      document.addEventListener("mousemove", this.moveHandler, true);
+      document.addEventListener("mouseup", this.upHandler, true);
+    } else if (document.attachEvent) {
+      this.refs.thisMount.setCapture();
+      this.refs.thisMount.attachEvent("onmousemove", this.moveHandler);
+      this.refs.thisMount.attachEvent("onmouseup", this.upHandler);
+      this.refs.thisMount.attachEvent("onlosecapture", this.upHandler);
+    }
+
+    if (e.preventDefault) e.preventDefault();
+    else e.returnValue = false;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='App-header App'>
+          <span>Drag Playground, hello React ! :D</span>
+        </div>
+        <div ref='thisMount' onMouseDown={this.onDrag} style={{width: 854, height: 565, position: 'absolute', zIndex: 200}}>
+          <div style={{height: 80, cursor: 'move', backgroundColor: '#d1f4ff', textAlign: 'center', lineHeight: '80px'}}>
+            <span>Drag here !</span>
+          </div>
+          <div>
+            <iframe width="854" height="485"
+                    src="https://www.youtube.com/embed/5XRaMP4QPwU"
+                    frameBorder="0" allowFullScreen>
+            </iframe>
+          </div>          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MotionBlock;
